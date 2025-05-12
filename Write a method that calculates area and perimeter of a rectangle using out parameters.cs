f23bbda6498b61c1fccd9dfec70b355bdb6ca0cc
AlsanKhadka <alsankhadka18@gmail.com>
2025-05-13 04:45:46 +0545

    Add files via upload

---
+++ Write a method that calculates area and perimeter of a rectangle using out parameters.cs
@@ -0,0 +1,21 @@
+﻿//Write a method that calculates area and perimeter of a rectangle using out parameters.
+public class OutUsages
+{
+    public static void Main()
+    {
+        int l, b,area,perimeter;
+       
+        AreaParameter(out l, out b, out area, out perimeter);
+        Console.WriteLine($"The area of the triangle is {area}");
+        Console.WriteLine($"The perimeter of the triangle is {perimeter}");
+    }
+    public static void AreaParameter(out int length,out int breadth, out int area1, out int perimeter1)
+    {
+        Console.Write("Enter your length ");
+        length = int.Parse(Console.ReadLine());
+        Console.Write("Enter your breadth ");
+       breadth = int.Parse(Console.ReadLine());
+        area1 = length * breadth;
+        perimeter1 = 2*(length + breadth);
+    }
+}
---
+++ Write a method that swaps two integers using ref.cs
@@ -0,0 +1,17 @@
+﻿//Write a method that swaps two integers using ref.
+public class Program
+{
+    public static void Main()
+    {
+        int a = 30, b = 20;
+        Console.WriteLine($"The value of a is {a} and the value of b is {b}");
+        Swap(ref a, ref b);
+        Console.WriteLine($"The value of a is {a} and the value of b is {b}");
+    }
+    public static void Swap(ref int num1, ref int num2)
+    {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+}

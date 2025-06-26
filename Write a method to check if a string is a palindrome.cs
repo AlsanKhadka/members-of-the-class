2316b89e67b9fb226e8b20480642a977a98dacda
AlsanKhadka <alsankhadka18@gmail.com>
2025-06-26 22:01:11 +0545

    Add files via upload

---
+++ Write a method to check if a string is a palindrome.cs
@@ -0,0 +1,37 @@
+﻿//Write a method to check if a string is a palindrome.
+namespace Namespace1
+{
+    public class Home
+    {
+       public bool IsPalindrome(string name)
+        {
+            name= name.ToLower();
+            string reverseName = "";
+            for(int i = 0; i < name.Length; i++)
+            {
+         reverseName= name[i]+reverseName;
+            }
+            if(reverseName == name)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static void Main()
+        {
+            Home home = new Home();
+            if (home.IsPalindrome("mom"))
+            {
+                Console.WriteLine($"The name is palindrome ");
+            }
+            else
+            {
+                Console.WriteLine($"The name is not palindrome ");
+            }
+        }
+    }
+
+}
